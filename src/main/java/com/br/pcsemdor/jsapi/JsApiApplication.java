94214eac171b32781abb5713bc740eb0e8bf85f2
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-01-31 09:52:47 -0300

    commit inicial

---
+++ src/main/java/com/br/pcsemdor/jsapi/JsApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.pcsemdor.jsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/pcsemdor/jsapi/JsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pcsemdor.jsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
